cc27c8d1b7d8c787fdd79f64cfa7faa84c17388b
heo <admin@admin.com>
2025-06-05 23:47:12 +0900

    commit

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -68,3 +68,15 @@ CSRF_TRUSTED_ORIGINS = [
     "http://52.79.37.117",
     "http://52.79.37.117:8080",
 ]
+
+#dev_10
+CORS_ORIGIN_ALLOW_ALL = True  # 어떠한 출처든 상관없이 정보를 공유
+
+# 다른 도메인에서의 API 접근 허용
+# 정확한 주소 필요, 포트 포함
+# CORS_ORIGIN_ALLOW_ALL = True 이면 아래는 필요 없음
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     "http://localhost:5173",  # 프론트 도메인
+# ]
